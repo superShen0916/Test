a46b709ed001a0f25a04fe76e0363414cf3055c4
shenpeng <1197921545@qq.com>
2020-05-08 16:41:43 +0800

    Revert "test2"
    This reverts commit 31cd6aa957bdb3b2964561814096890688806317.

--- src/main/java/thread/SynchronizedMethod.java
+++ src/main/java/thread/SynchronizedMethod.java
@@ -27,7 +27,6 @@ import java.util.concurrent.Callable;
  *               synchronized (s){...} |
  * 
  *               //test
- *               //test 2
  * @Author: shenpeng
  * @Date: 2019-09-04
  */
